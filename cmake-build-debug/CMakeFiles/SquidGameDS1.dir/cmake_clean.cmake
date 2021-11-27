2c160db5b1aac81b0ebdf39ae15370f92c837256
TomatoRage <61627067+TomatoRage@users.noreply.github.com>
2021-11-27 20:36:33 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/SquidGameDS1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SquidGameDS1.dir/library1.cpp.obj"
+  "CMakeFiles/SquidGameDS1.dir/main1.cpp.obj"
+  "SquidGameDS1.exe"
+  "SquidGameDS1.exe.manifest"
+  "SquidGameDS1.pdb"
+  "libSquidGameDS1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SquidGameDS1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SquidGameDS1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SquidGameDS1.
+# This may be replaced when dependencies are built.
