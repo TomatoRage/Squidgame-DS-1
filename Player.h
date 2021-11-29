d84edf698160dc686ec81f5fa7b96d253965aea8
mhmdb <mhmd.best.1998>
2021-11-29 15:17:05 +0200

    little

--- Player.h
+++ Player.h
@@ -1,6 +1,6 @@
 #ifndef SQUIDGAMEDS1_PLAYER_H
 #define SQUIDGAMEDS1_PLAYER_H
-
+#include "stdlib.h"
 #include "BinarySearchTree.h"
 
 class Group;
@@ -14,22 +14,47 @@ class Player {
 
 public:
 
-    Player(int Id,int Lvl) : Id(ID) , lvl(Level)  {};
+    Player(int PlayerId,int Lvl) : ID(PlayerId) , Level(Lvl) , AllGroups(NULL) , UsedGroups(NULL) {};
     Player(int Id) ;
     ~Player();
-    void UpdateAllGroup(Group* Group);
-    void UpdateUsedGroup(Group* Group);
+    void UpdateAllGroup(Group* Group){
+        AllGroups = Group;
+    }
+    void UpdateUsedGroup(Group* Group){
+        UsedGroups = Group;
+    }
     void UpdateLevel(int Level_inc) {
         Level += Level_inc;
     }
+
     int GetLevel() {
         return Level;
     }
+
     int GetID() {
         return ID;
     }
     //TODO:Implement Methods
+
+    friend bool operator>(const Player& c1, const Player& c2);
+    friend bool operator<(const Player& c1, const Player& c2);
+    friend bool operator==(const Player& c1, const Player& c2);
 };
+
+bool operator>(const Player& p1, const Player& p2){
+    if(p1.Level == p2.Level){
+        return p1.ID>p2.ID;
+    }
+    return p1.Level > p2.Level;
+}
+bool operator<(const Player& p1, const Player& p2){
+    return !(p1>p2);
+}
+
+bool operator==(const Player& p1, const Player& p2){
+    return (p1.ID == p2.ID);
+}
+
 
 
 #endif //SQUIDGAMEDS1_PLAYER_H
